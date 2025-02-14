3ec6154cc30d4cbb43a316b5f9a88cd3c29763fa
jill bhatt <bhattjill@gmail.com>
2025-02-14 18:48:44 +0530

    Added cors for handling swagger requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv/config');
+const cors = require('cors');
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./swagger');
@@ -7,10 +8,13 @@ const appRoutes = require('./routes');
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(appRoutes);
+
+app.set('trust proxy', 1);
 
 app.use(
     '/',
